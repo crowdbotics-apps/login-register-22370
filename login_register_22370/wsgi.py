63c669c3658f463166c7e9ba47fcee800b60896a
Crowdbotics <team@crowdbotics.com>
2020-11-07 01:56:19 +0000

    Update models

--- login_register_22370/wsgi.py
+++ login_register_22370/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login_register_22370.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login_register_22370.settings")
 
 application = get_wsgi_application()
